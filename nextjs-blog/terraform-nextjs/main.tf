ae2e8f4b2d08b1c813e2c08a6f3cf0e832c01251
innocent <innocentgodwin580@yahoo.com>
2025-08-29 23:10:53 +0930

    crreated cloudfront distribution file

--- nextjs-blog/terraform-nextjs/main.tf
+++ nextjs-blog/terraform-nextjs/main.tf
@@ -72,3 +72,57 @@ resource "aws_s3_bucket_policy" "website_policy" {
   })
 
 }
+
+# Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+    comment = "OIA for Next.js portfolio site"
+  
+}
+
+# cloudfront distribution
+# CloudFront distribution
+resource "aws_cloudfront_distribution" "nextjs_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id   = "inno-s3-nextjs-portfolio-bucket"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Next.js portfolio site"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "inno-s3-nextjs-portfolio-bucket"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
