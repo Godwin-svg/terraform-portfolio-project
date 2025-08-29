1e01db75f481bdd2e8eec136400ea4ac7117d8e9
innocent <innocentgodwin580@yahoo.com>
2025-08-29 22:06:42 +0930

    s3 policy created

--- nextjs-blog/terraform-nextjs/main.tf
+++ nextjs-blog/terraform-nextjs/main.tf
@@ -1,1 +1,74 @@
 # create s3 bucket
+resource "aws_s3_bucket" "website" {
+  bucket = "inno-123-nextjs-website-bucket"
+
+  tags = {
+    Name = "inno-123-nextjs-website-bucket"
+  }
+
+}
+
+# s3 bucket website configuration
+resource "aws_s3_bucket_website_configuration" "website-config" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+
+}
+
+# bucket ownership control
+resource "aws_s3_bucket_ownership_controls" "bucket-ownership-controls" {
+  bucket = aws_s3_bucket.website.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+
+}
+
+# allow bucket public access 
+resource "aws_s3_bucket_public_access_block" "allow-public-access" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+}
+
+
+# bucket acl
+resource "aws_s3_bucket_acl" "bucket-acl" {
+  bucket = aws_s3_bucket.website.id
+
+  depends_on = [aws_s3_bucket_ownership_controls.bucket-ownership-controls, aws_s3_bucket_public_access_block.allow-public-access]
+
+  acl = "public-read"
+
+}
+
+# s3 policy
+resource "aws_s3_bucket_policy" "website_policy" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
+  })
+
+}
